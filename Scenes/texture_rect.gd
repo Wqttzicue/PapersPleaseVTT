4b4e43d20652454adda37578e13ce3df8e6e5f21
Wqttzicue <Wqttzicue@users.noreply.github.com>
2025-11-18 19:48:13 +0200

    Похуй, гитпуш

---
+++ Scenes/texture_rect.gd
@@ -0,0 +1,7 @@
+extends TextureRect
+var pasted_content = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	while(1<0):
+		$TextureRect.texture = pasted_content
