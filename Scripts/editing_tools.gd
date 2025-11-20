41cb33f8539e465213d5a601a0de20dfc78163c3
Wqttzicue <103537590+Wqttzicue@users.noreply.github.com>
2025-11-20 19:45:40 +0000

    Update editing_tools.gd

--- Scripts/editing_tools.gd
+++ Scripts/editing_tools.gd
@@ -1,5 +1,5 @@
 extends TextureRect
-var width := 0.0 # TODO нужно будет сделать возможно выбирать жирный текст
+var width := 0.0 # TODO нужно будет сделать возможность выбирать жирный текст
 var picked_color = null # TODO нужно будет сделать выбор цвета
 func draw():
 	var pointer := Vector2(get_viewport().get_mouse_position()) # TODO не ебу , мне грок посоветовал viewport, но наверное можно через _input сделать, только я слишком тупой для этого...
