ca64e874d096f2fdd19ac5c318279bc213803521
Wqttzicue <Wqttzicue@users.noreply.github.com>
2025-11-20 21:37:43 +0200

    pointer coordinates variable
    {in} global_map.tscn
    [+] Pointer Coordinates Variable
    [+] Base function for drawing TBC

--- Scenes/texture_rect.gd
+++
@@ -1,7 +0,0 @@
-extends TextureRect
-var pasted_content = null
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	while(1<0):
-		$TextureRect.texture = pasted_content
---
+++ Scripts/editing_tools.gd
@@ -0,0 +1,9 @@
+extends TextureRect
+var width := 0.0 # TODO нужно будет сделать возможно выбирать жирный текст
+var picked_color = null # TODO нужно будет сделать выбор цвета
+func draw():
+	var pointer := Vector2(get_viewport().get_mouse_position()) # TODO не ебу , мне грок посоветовал viewport, но наверное можно через _input сделать, только я слишком тупой для этого...
+	if pointer != null:
+		draw_circle(pointer, width, picked_color, true)
+	else:
+		return
