4442c7b1fdd302f8a2d8110867a241dc4c6057b6
Wqttzicue <Wqttzicue@users.noreply.github.com>
2025-11-22 21:33:26 +0200

    packed arrays added
    half-way to implement working drawing with packed arrays.

---
+++ Scripts/draw_data.gd
@@ -0,0 +1,5 @@
+class_name DrawData
+#Class Variables
+@export var colored_points : Array[Vector2]
+@export var color := Color.RED
+@export var width := 100.0
--- Scripts/editing_tools.gd
+++ Scripts/editing_tools.gd
@@ -1,19 +1,12 @@
 extends TextureRect
-var width := 10 # TODO нужно будет сделать возможность выбирать жирный текст
-var picked_color := Color.GREEN # TODO нужно будет сделать выбор цвета
-func draw():
-	var pointer := Vector2(get_viewport().get_mouse_position()) # TODO не ебу , мне грок посоветовал viewport, но наверное можно через _input сделать, только я слишком тупой для этого...
-	if pointer != null:
-		draw_circle(pointer, width, picked_color, true)
-	else:
-		return
+var draw_data := DrawData.new()
 
-func _process(delta: float):
+func _process(delta):
+	if Input.is_action_pressed("LMB"):
 		queue_redraw()
 
 func _draw():
+	var pointer := Vector2(get_viewport().get_mouse_position())
 	if Input.is_action_pressed("LMB"):
-		print("drawing..")
-		draw()
-	print("_draw is working")
-	
+		draw_data.colored_points.append(pointer)
+		draw_polygon(draw_data.colored_points, [draw_data.color])
